f8ab14d31a284dd73da50c5177cb687a60a969a1
Matt McKnett <mmcknett@gmail.com>
2022-11-17 15:34:28 -0800

    feat: Implement first 3 ACs

--- src/index.test.ts
+++ src/index.test.ts
@@ -1,16 +1,44 @@
-import { sayIt } from ".";
+import { Form, TextField } from ".";
 
-describe('The Person', () => {
-  it('Has a sayIt method that outputs the expected string', () => {
-    let mike = {
-      age: 24, 
-      name:"Mikey", 
-      say: function() { 
-          return "My name is " + this.name + 
-                 " and I'm " + this.age + " years old!"
-      }
-    }
+describe('The Form', () => {
+  it('Can have a text field added to it', () => {
+    // const textField = new TextField('anyLabel', 'anyName', )
+    const form = new Form('someActionUrl');
+    form.addTextField('anyLabel', 'anyName', 'anyType')
 
-    expect(sayIt(mike)).toBe("My name is Mikey and I'm 24 years old!");
+    expect(form.textFields.length).toEqual(1);
   });
+
+  it('Can find a text field by its name', () => {
+    const form = new Form('someActionUrl');
+    form.addTextField('anyLabel1', 'name-1', 'anyType')
+    form.addTextField('anyLabel2', 'name-2', 'anyType')
+
+    const actual = form.findTextFieldByName('name-1');
+
+    expect(actual!.name).toBe('name-1');
+  });
+
+  it('Find returns something falsy if there is no textfield of a particular name', () => {
+    const form = new Form('someActionUrl');
+    form.addTextField('anyLabel1', 'name-1', 'anyType')
+    form.addTextField('anyLabel2', 'name-2', 'anyType')
+
+    const actual = form.findTextFieldByName('name-3');
+
+    expect(actual).toBeFalsy;
+  });
+
+  it('Can set a value of a named field', () => {
+    const form = new Form('someActionUrl');
+    form.addTextField('anyLabel1', 'name-1', 'anyType')
+    form.addTextField('anyLabel2', 'name-2', 'anyType')
+
+    form.setTextFieldValue('name-1', 'aNewValue');
+
+    const tf = form.findTextFieldByName('name-1');
+    expect(tf!.value).toBe('aNewValue');
+  });
+
+  
 });
--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,48 @@
 
-export interface Person {
-  age: number,
-  name: string,
-  say(): string
-}
+export class Form {
+  actionURL: string
+  textFields: TextField[]
+  lastOrder: number = 0
 
-let mike = {
-  age: 25, 
-  name:"Mike", 
-  say: function() { 
-      return "My name is " + this.name + 
-             " and I'm " + this.age + " years old!"
+  constructor(actionURL: string) {
+    this.actionURL = actionURL;
+    this.textFields = []
+  }
+
+  addTextField(label: string, name: string, type: string, value?: string) {
+    // NOTE: this.lastOrder may interfere with later ACs.
+    this.textFields.push(new TextField(label, name, this.lastOrder, type, value));
+    this.lastOrder += 1;
+  }
+
+  findTextFieldByName(name: string) {
+    const textField = this.textFields.find(tf => tf.name === name);
+    return textField;
+  }
+
+  setTextFieldValue(name: string, value: string) {
+    const textField = this.findTextFieldByName(name);
+    if (textField) {
+      textField.value = value;
+    }
   }
 }
 
-export function sayIt(person: Person) {
-  return person.say();
-}
 
-console.log(sayIt(mike))
+export class TextField {
+  label: string
+  name: string
+  order: number
+  type: string
+  value?: string
+
+  constructor(label: string, name: string, order: number, type: string, value?: string) {
+    this.label = label;
+    this.name = name;
+    this.order = order;
+    this.type = type;
+    if (value) {
+      this.value = value
+    }
+  }
+}
